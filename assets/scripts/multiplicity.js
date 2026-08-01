a5dc8c7dbd88e72d97c51f5482f71df0d4077fd4
Sim-karta <simonenkonikita561@gmail.com>
2026-08-02 02:31:55 +0300

    Add checkMultiplicity button

--- assets/scripts/multiplicity.js
+++ assets/scripts/multiplicity.js
@@ -1,10 +1,14 @@
+const checkMultiplicityBtn = document.querySelector(".check-multiplicity-btn");
+
 function checkMultiplicity(num1, num2) {
     return num1 % num2 === 0;
 }
 
-console.log(
-    checkMultiplicity(
-        prompt("Введіть перше число"),
-        prompt("Введіть друге число"),
-    ),
-);
+checkMultiplicityBtn.addEventListener("click", () => {
+    console.log(
+        checkMultiplicity(
+            prompt("Введіть перше число"),
+            prompt("Введіть друге число"),
+        ),
+    );
+});
