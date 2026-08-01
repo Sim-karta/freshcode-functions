4d5352bacabdbc2b3fe428b8a278ced173e7da7c
Sim-karta <simonenkonikita561@gmail.com>
2026-08-01 23:19:41 +0300

    Add check button

--- assets/scripts/age.js
+++ assets/scripts/age.js
@@ -1,5 +1,9 @@
+const checkAgeBtn = document.querySelector(".check-age-btn");
+
 function isWorkingAgePerson(age) {
     return age >= 16 && age <= 64;
 }
 
-console.log(isWorkingAgePerson(prompt("Введіть вік людини - ")));
+checkAgeBtn.addEventListener("click", () => {
+    console.log(isWorkingAgePerson(prompt("Введіть вік людини - ")));
+});
